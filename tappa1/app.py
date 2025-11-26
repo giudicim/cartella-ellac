9c1088125d2421080cfa9f7eba8006283750de6d
giudicim <magagilu77@gmail.com>
2025-11-26 10:50:18 +0100

    ritorno

--- tappa1/app.py
+++ tappa1/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Benvenuti!"
-
-
+    return "Benvenuti nel tutorial Flask!"
 
 if __name__ == '__main__':
     app.run(debug=True)
